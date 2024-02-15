1776420b2d2b11e310e2df6971c1ab9dd8bfa49f
Murod-de <murad101693@gmail.com>
2024-02-15 08:37:52 +0500

    all exam file

---
+++ Three/script.js
@@ -0,0 +1,11 @@
+const array1 = [1, 2, 2];
+
+function arrFunc(arr) {
+    let result = 0;
+    for(let num of arr) {
+        console.log(result = num);
+    }
+    
+}
+console.log(arrFunc(array1));
+
